5e4df1def582a261581ccc609e69290936513e26
Wilson <l7960261@gmail.com>
2019-05-29 14:21:01 +0800

    Add _orders

--- src/app/table-demo/table-demo.component.ts
+++ src/app/table-demo/table-demo.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { orders } from './_orders';
 
 @Component({
   selector: 'app-table-demo',
@@ -9,37 +10,19 @@ export class TableDemoComponent implements OnInit {
   searchValue = '';
   sortName: string | null = null;
   sortValue: string | null = null;
-  listOfFilterAddress = [{ text: 'London', value: 'London' }, { text: 'Sidney', value: 'Sidney' }];
   listOfSearchAddress: string[] = [];
-  listOfData: Array<{ name: string; age: number; address: string;[key: string]: string | number }> = [
-    {
-      name: 'John Brown',
-      age: 32,
-      address: 'New York No. 1 Lake Park'
-    },
-    {
-      name: 'Jim Green',
-      age: 42,
-      address: 'London No. 1 Lake Park'
-    },
-    {
-      name: 'Joe Black',
-      age: 32,
-      address: 'Sidney No. 1 Lake Park'
-    },
-    {
-      name: 'Jim Red',
-      age: 32,
-      address: 'London No. 2 Lake Park'
-    }
-  ];
+  listOfData: Array<{ CreatedOn: string; Id: string; In: number; Out: number; }> = orders;
   listOfDisplayData = [...this.listOfData];
+  mapOfSort: { [key: string]: any } = {
+    CreatedOn: null,
+    In: null,
+    Out: null
+  };
 
   constructor() { }
 
   ngOnInit() {
   }
-
 
   reset(): void {
     this.searchValue = '';
@@ -49,31 +32,28 @@ export class TableDemoComponent implements OnInit {
   sort(sortName: string, value: string): void {
     this.sortName = sortName;
     this.sortValue = value;
-    this.search();
-  }
-
-  filterAddressChange(value: string[]): void {
-    this.listOfSearchAddress = value;
+    for (const key in this.mapOfSort) {
+      this.mapOfSort[key] = key === sortName ? value : null;
+    }
     this.search();
   }
 
   search(): void {
-    const filterFunc = (item: { name: string; age: number; address: string }) => {
-      return (
-        (this.listOfSearchAddress.length
-          ? this.listOfSearchAddress.some(address => item.address.indexOf(address) !== -1)
-          : true) && item.name.indexOf(this.searchValue) !== -1
+    const listOfData = this.listOfData
+      .filter(item => item.Id.indexOf(this.searchValue) !== -1);
+
+    if (this.sortName && this.sortValue) {
+      this.listOfDisplayData = listOfData.sort((a, b) =>
+        this.sortValue === 'ascend'
+          ? a[this.sortName!] > b[this.sortName!]
+            ? 1
+            : -1
+          : b[this.sortName!] > a[this.sortName!]
+            ? 1
+            : -1
       );
-    };
-    const data = this.listOfData.filter((item: { name: string; age: number; address: string }) => filterFunc(item));
-    this.listOfDisplayData = data.sort((a, b) =>
-      this.sortValue === 'ascend'
-        ? a[this.sortName!] > b[this.sortName!]
-          ? 1
-          : -1
-        : b[this.sortName!] > a[this.sortName!]
-          ? 1
-          : -1
-    );
+    } else {
+      this.listOfDisplayData = listOfData;
+    }
   }
 }
