ed4e3305eac08dcd917ba523d015b3a961b9db63
Wilson <l7960261@gmail.com>
2019-05-29 11:51:47 +0800

    Remove theme.less

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { EmptyRouteComponent } from './empty-route/empty-route.component';
 import { NgZorroAntdModule, NZ_I18N, en_US } from 'ng-zorro-antd';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { registerLocaleData } from '@angular/common';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { EmptyRouteComponent } from './empty-route/empty-route.component';
 import en from '@angular/common/locales/en';
 
 registerLocaleData(en);
